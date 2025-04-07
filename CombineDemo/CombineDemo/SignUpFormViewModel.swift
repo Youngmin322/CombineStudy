26ef2ba4e6d8e0cd51536fe57bc876efae86463c
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-07 15:38:17 +0900

    Combine 에러처리 - 스트림의 결과 값을 Result 와 Error 로 반환하기

--- CombineDemo/CombineDemo/SignUpFormViewModel.swift
+++ CombineDemo/CombineDemo/SignUpFormViewModel.swift
@@ -40,7 +40,11 @@ class SignUpFormViewModel: ObservableObject {
                 return username
             }
             .flatMap { username -> AnyPublisher<Bool, Never> in
-                return self.authenticationService.checkUserNameAvailableNaive(userName: username)
+                return self.authenticationService.checkUserNameAvailablePublisher(userName: username)
+                    .catch { error in
+                        return Just(false)
+                    }
+                    .eraseToAnyPublisher()
             }
             .receive(on: DispatchQueue.main)
             .share()
