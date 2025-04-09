8d7a41f43266b77c0b8f56bb53649e641a762167
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-09 15:52:35 +0900

    라이브 도서 목록 실습: 컴바인 ( 구독, 해지 ) 및 self 참조를 약한 참조로 변경하여 메모리 누수 방지

--- CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/LiveBooksListViewWithCombine.swift
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/LiveBooksListViewWithCombine.swift
@@ -6,12 +6,54 @@
 //
 
 import SwiftUI
+import Combine
+import FirebaseFirestore
+
+private class BookListViewModel: ObservableObject {
+    @Published var books: [Book] = []
+    @Published var errorMessage: String?
+    
+    private var db = Firestore.firestore()
+    private var cancellable: AnyCancellable?
+    
+    fileprivate func subscribe() {
+        cancellable = db.collection("books").snapshotPublisher()
+            .map { querySnapshot in
+                querySnapshot.documents.compactMap { documentSnapshot in
+                    try? documentSnapshot.data(as: Book.self)
+                }
+            }
+            .catch { error in
+                self.errorMessage = error.localizedDescription
+                return Just([Book]()).eraseToAnyPublisher()
+            }
+            .replaceError(with: [Book]())
+            .handleEvents(receiveCancel: {
+                print("Cacelled")
+            })
+            .assign(to: \.books, on: self)
+    }
+    
+    fileprivate func unsubscribe() {
+        cancellable?.cancel()
+        cancellable = nil
+    }
+}
 
 struct LiveBooksListViewWithCombine: View {
-  var body: some View {
-    VStack {
-      EmptyView()
+    @StateObject private var viewModel = BookListViewModel()
+    
+    var body: some View {
+        List(viewModel.books) { book in
+            Text(book.title)
+        }
+        .navigationTitle("Book List")
+        .onAppear {
+            viewModel.subscribe()
+        }
+        .onDisappear {
+            viewModel.unsubscribe()
+        }
     }
-    .navigationTitle("Book Live")
-  }
 }
+
