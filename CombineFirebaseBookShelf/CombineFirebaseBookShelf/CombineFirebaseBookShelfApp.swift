397457a17ef6f61ea7a0ba41634cba3d118945d3
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-09 09:53:39 +0900

    CombineFirebaseBookShelf 프로젝트 파일 생성 및 Firebase 패키지 추가

---
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/CombineFirebaseBookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFirebaseBookShelfApp.swift
+//  CombineFirebaseBookShelf
+//
+//  Created by 조영민 on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineFirebaseBookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
